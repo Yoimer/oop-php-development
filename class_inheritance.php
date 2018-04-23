0cc93c1a96aa8b3e7e7f8c47a1ce0f383a838f09
Yoimer <ydrf19@gmail.com>
2018-04-23 14:58:59 -0400

    extend and override

--- class_inheritance.php
+++ class_inheritance.php
@@ -2,6 +2,8 @@
 
 class User {
 
+  var $is_admin = false;
+
   var $first_name;
   var $last_name;
   var $username;
@@ -13,6 +15,22 @@ class User {
 }
 
 class Customer extends User {
+
+  var $city;
+  var $state; // or province
+  var $country;
+
+  function location() {
+    return $this->city . ", " . $this->state . ", " . $this->country;
+  }
+}
+
+class AdminUser extends User {
+  var $is_admin = true; //overrides class User
+
+  function full_name() {
+    return $this->first_name . " " . $this->last_name . " (Admin)";
+  }
 }
 
 $u = new User;
@@ -24,9 +42,16 @@ $c = new Customer;
 $c->first_name = 'George';
 $c->last_name = 'Costanza';
 $c->username = 'gcostanza';
+$c->city = 'New York';
+$c->state = 'New York';
+$c->country = 'United States';
 
 echo $u->full_name() . '<br />';
 echo $c->full_name() . '<br />';
+
+echo $c->location() . '<br />';
+// echo $u->location() . '<br />'; // no method error
+
 
 echo get_parent_class($u) . '<br />';
 echo get_parent_class($c) . '<br />';
