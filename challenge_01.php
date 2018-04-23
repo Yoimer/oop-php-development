cb1f847cde6e3a2a1a0bd7e75179fabe6f2878da
Yoimer <ydrf19@gmail.com>
2018-04-23 00:20:42 -0400

    challenge_01 description

---
+++ challenge_01.php
@@ -0,0 +1,44 @@
+<?php
+
+    class Bicycle {
+
+        //attributes or properties
+        var brand = '';
+        var model = '';
+        var year = -1;
+        var description = '';
+        var weight_kg = -1;
+
+        // methods or functions
+        function name() {
+            // code goes here
+            //brand, number and year put'em together
+            // in a string of your choosing
+            // to output a simple name for this bicycle
+        }
+
+        function weight_lbs() {
+            // code goes here
+            // convert from kg into pounds
+            // 1kg = 2.2046226218lbs
+        }
+
+        function set_weight_lbs() {
+            // code goes here
+            //convert weight from pounds to kg 
+            //and save the value in weight_kg
+        }
+    }
+    // Instantiate a few different objects
+    // Setting and reading their properties
+    // Call all the methods you've created
+
+
+
+
+
+
+
+
+
+?>
